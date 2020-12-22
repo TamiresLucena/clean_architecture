7604ef36764f27162f12f42e12a453709c79259f
Tamires Lucena <tamiresb.lucena@gmail.com>
2020-12-21 23:19:24 -0300

    feat: verify e-mail address when creating user

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,29 +1,46 @@
+import { Either, right, left } from '../shared/either'
+import { InvalidEmailError } from './errors/invalid-email-error'
+
 export class Email {
-  static validate (email: string) : boolean {
-    if (!email) {
-      return false
-    }
-    if (email.length > 320) {
-      return false
+    private readonly email: string
+
+    constructor (email: string) {
+      this.email = email
     }
 
-    const emailRegex = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+    static create (email: string): Either<InvalidEmailError, Email> {
+      if (Email.validate(email)) {
+        return right(new Email(email))
+      }
 
-    if (!emailRegex.test(email)) {
-      return false
+      return left(new InvalidEmailError())
     }
 
-    const [local, domain] = email.split('@')
-    if (local.length > 64 || local.length === 0) {
-      return false
-    }
-    if (domain.length > 255 || domain.length === 0) {
-      return false
-    }
+    static validate (email: string) : boolean {
+      if (!email) {
+        return false
+      }
+      if (email.length > 320) {
+        return false
+      }
 
-    const domainParts = domain.split('.')
-    if (domainParts.some((part) => { return part.length > 63 })) return false
+      const emailRegex = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
 
-    return true
-  }
+      if (!emailRegex.test(email)) {
+        return false
+      }
+
+      const [local, domain] = email.split('@')
+      if (local.length > 64 || local.length === 0) {
+        return false
+      }
+      if (domain.length > 255 || domain.length === 0) {
+        return false
+      }
+
+      const domainParts = domain.split('.')
+      if (domainParts.some((part) => { return part.length > 63 })) return false
+
+      return true
+    }
 }
