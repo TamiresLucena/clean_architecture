5ac87e35f2252d1e8ee9f75d9f8e4aa664c0663a
Tamires Lucena <tamiresb.lucena@gmail.com>
2021-02-01 22:27:03 -0300

    test: web controller should return 201 when user is created

---
+++ test/web-controllers/register-user-controller.spec.ts
@@ -0,0 +1,24 @@
+import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
+import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
+import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
+import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { UserData } from '@/entities'
+import { RegisterUserController } from '@/web-controllers/register-user-controller'
+
+describe('Register user web controller', () => {
+  test('should return status code 201 when request contains valid user data', async () => {
+    const request: HttpRequest = {
+      body: {
+        name: 'Any name',
+        email: 'any@mail.com'
+      }
+    }
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase : RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const controller: RegisterUserController = new RegisterUserController(usecase)
+    const response: HttpResponse = await controller.handle(request)
+    expect(response.statusCode).toEqual(201)
+    expect(response.body).toEqual(request.body)
+  })
+})
